38517ab1a6c6669f1d27fd18eaed604272aadc41
lyk <lykwowlol@gmail.com>
2019-07-15 18:10:07 +0800

    fix: enable-electron-vibrancy.js

--- enable-electron-vibrancy.js
+++ enable-electron-vibrancy.js
@@ -1,1 +1,10 @@
-nodeRequire('electron').remote.getCurrentWindow().setVibrancy('ultra-dark');
+w = nodeRequire('electron').remote.getCurrentWindow();
+w.setBackgroundColor('#00000000');
+w.setVibrancy('ultra-dark');
+const width = w.getBounds().width;
+w.setBounds({
+    width: width + 1,
+});
+w.setBounds({
+    width,
+});
